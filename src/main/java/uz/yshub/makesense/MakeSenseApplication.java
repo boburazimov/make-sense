9cae40ddeb021804f771e2e11af0ba9c03e7a222
bobur <uzbek.4103@gmail.com>
2022-08-29 11:49:24 +0500

    init

---
+++ src/main/java/uz/yshub/makesense/MakeSenseApplication.java
@@ -0,0 +1,13 @@
+package uz.yshub.makesense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeSenseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/yshub/makesense/MakeSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.yshub.makesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
